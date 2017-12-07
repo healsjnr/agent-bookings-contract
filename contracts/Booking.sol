d40f0ffe3690bea1fba73c304f403f27f5696423
David Healy <healsjnr@gmail.com>
2017-12-08 08:21:26 +1100

    more tidying

--- contracts/Booking.sol
+++ contracts/Booking.sol
@@ -17,6 +17,12 @@ contract Booking {
     address supplier;
     uint checkInEpochSeconds;
     uint checkOutEpochSeconds;
+    // TODO 
+    // Remove data and just have a finalised_at date
+    // add balances for customer, supplier, agent
+    // This is kind of like a token. All three parties can call balanceOf(_tokenId) and recieve their balance
+    // The customer balance goes to 0 once the finalised at has been reached
+    // The others go to zero on draw down
   }
   
   address public bookingProvider; //Must be set in the constructor of the contract
@@ -26,7 +32,7 @@ contract Booking {
   // Many probolems with this being the public method. Customer can effectively set all the values
   // Probably needs to be two part. Create by us, pay by customer.
   function createBooking(address customer, uint bookingId, uint bookingValue, bool isRefundable, address supplier, uint checkInEpochSeconds, uint checkOutEpochSeconds) returns (bool) {
-    // return false if booking Id already exists
+    require(bookings[bookingId].bookingId == 0);
     BookingDetails memory bookingDetails = BookingDetails(bookingId, bookingValue, isRefundable, BookingState.Creating, customer, supplier, checkInEpochSeconds, checkOutEpochSeconds);
     bookings[bookingId] = bookingDetails;
     return true;
@@ -36,8 +42,8 @@ contract Booking {
     BookingDetails storage bookingDetails = bookings[bookingId];
     require(bookingDetails.bookingId != 0);
     require(bookingDetails.bookingState == BookingState.Creating);
-    require(msg.sender == bookingDetails.customer); //do this via a modifier?
-    if (msg.value != bookingDetails.bookingValue) throw;
+    require(msg.sender == bookingDetails.customer);
+    require(msg.value == bookingDetails.bookingValue);
     // TODO Do the transaction dance. 
     bookingDetails.bookingState = BookingState.Booked;
     return true;
@@ -61,6 +67,7 @@ contract Booking {
     return this.balance;
   }
 
+  //function balanceOf(_tokenId)
   //function getBookingState(string bookingId) constant returns (BookingState) { }
   //
   //function quoteCancellation(string bookingId) constant returns (uint) { }
